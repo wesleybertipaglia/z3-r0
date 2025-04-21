f71422a164618703a5f897696192a9e04a837272
wesleybertipaglia <wesleybertipaglia@gmail.com>
2025-04-21 18:19:26 -0300

    feat: add sound playback functionality; integrate sound effect on bot message response

--- src/hooks/useBot.ts
+++ src/hooks/useBot.ts
@@ -3,6 +3,7 @@ import { useTranslation } from "react-i18next";
 import { MessageType } from "../types/message";
 import { useMessage } from "./useMessage";
 import { useCommands } from "./useCommands";
+import { useSound } from "./useSound";
 
 let msgId = 0;
 
@@ -11,6 +12,7 @@ export function useBot() {
     const { messages, addMessage, initialized, randomMessage } = useMessage();
     const { resolveCommand } = useCommands();
     const [isTyping, setIsTyping] = useState(false);
+    const { play } = useSound();
 
     useEffect(() => {
         if (!initialized) return;
@@ -18,7 +20,7 @@ export function useBot() {
         if (messages.length === 0) {
             const welcomeMessages = t("welcome", { returnObjects: true }) as string[];
             const randomWelcome = welcomeMessages[Math.floor(Math.random() * welcomeMessages.length)];
-            addMessage({ id: msgId++, from: "bot", content: randomWelcome, type: "info" });
+            onBotMessage({ type: "info", content: randomWelcome });
         }
 
         const minInterval = 30000;
@@ -31,20 +33,20 @@ export function useBot() {
         }, randomInterval);
 
         return () => clearInterval(interval);
-    }, [initialized, messages, t, addMessage, randomMessage]);
+    }, [initialized, t]);
 
     function onUserMessage(text: string) {
         addMessage({ id: msgId++, from: "user", content: text });
-        onBotMessage(text);
+        const response = resolveCommand(text.trim());
+        onBotMessage({ type: response.type as MessageType, content: response.content });
     }
 
-    function onBotMessage(input: string) {
-        const response = resolveCommand(input.trim());
-
+    function onBotMessage({ type, content }: { type: MessageType; content: string }) {
         setIsTyping(true);
         setTimeout(() => {
-            addMessage({ id: msgId++, from: "bot", content: response.content, type: response.type as MessageType });
+            addMessage({ id: msgId++, from: "bot", content, type });
             setIsTyping(false);
+            play("pop.mp3");
         }, 1200);
     }
 
